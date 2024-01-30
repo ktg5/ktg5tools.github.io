507f27dbbb3007cffaac77866fd559aa5c6f2708
ktg5 <42522603+ktg5@users.noreply.github.com>
2024-01-29 23:18:55 -0800

    multittv: focus stream | more configs | mute on add | ui improvements

--- multittv/src/multittv.js
+++ multittv/src/multittv.js
@@ -2,6 +2,8 @@
 var def_config = {
     channels: [],
     THISISYOURTWITCHACCOUNTINFORMATION_DONTSHARETHIS: {},
+    welcomeMessage: false,
+    muteOnAdd: true,
 }
 
 var storageKey = 'multittv-settings';
@@ -28,7 +30,7 @@ function applyConfig() {
 }
 
 // Add stream
-function addStream(name) {
+async function addStream(name) {
     // Make sure channel isn't already added
     if (getStream(name)) {
         return alert('You already have this channel added IDIOT!!!');
@@ -37,9 +39,14 @@ function addStream(name) {
     if (streams.length == 9) {
         return alert('You can only have a maximum of 9 streams!');
     }
-    // Make iframe
+    // Make iframe & apply settings
+    var isMuted = false;
+    if (userConfig.muteOnAdd == true) {
+        isMuted = true;
+    }
     var stream = new Twitch.Player("stream-insert", {
-        channel: `${name}`
+        channel: `${name}`,
+        muted: isMuted
     });
     // Add to channel list
     document.querySelector('.menu-channels').insertAdjacentHTML('beforeend', 
@@ -60,14 +67,43 @@ function addStream(name) {
     </button>
     `
     )
-    // Add event listener for focusing
-    document.querySelector(`.menu-button.channel.${name.replace(/\d+/g, '')}`).addEventListener('click', async () => {
-        console.log(`focus ${name}`);
-    });
+
     // Add event listener for closing the button
     document.querySelector(`.menu-button.channel.${name.replace(/\d+/g, '')} .remove`).addEventListener('click', async () => {
         removeStream(name);
     });
+    // Add event listener for focusing
+    document.querySelector(`.menu-button.channel.${name.replace(/\d+/g, '')}`).addEventListener('click', async () => {
+        if (getStream(name)) {
+            if (document.querySelector(`#channel-focus iframe.${name.replace(/\d+/g, '')}`)) {
+                removeFocused();
+            } else {
+                let iframe = getStream(name).element;
+                let focusElmnt = document.querySelector('#channel-focus')
+                let channelContainer = document.querySelector('.channels-container');
+                // If there's a current focused stream
+                if (focusElmnt.querySelector('iframe')) {
+                    focusElmnt.querySelector('iframe').remove();
+                }
+                // Check if menu button has "focus" class name, & remove
+                if (document.querySelector('.menu-button.focus')) {
+                    document.querySelector('.menu-button.focus').classList.remove('focus');
+                }
+                // Make Twitch embed
+                new Twitch.Player("channel-focus", {
+                    channel: `${name}`
+                });
+                focusElmnt.querySelector('iframe').classList.add(name.replace(/\d+/g, ''))
+                // Add focus to menu button
+                getStream(name).menubutton.classList.add('focus');
+                // Add to focus div
+                if (!channelContainer.classList.contains('focus')) {
+                    channelContainer.classList.add('focus');
+                }
+            }
+        }
+    });
+
     // Add to channels list
     document.querySelector('channels').childNodes.forEach(element => {
         if (name == element.src.split('?channel=')[1].split('&')[0]) {
@@ -108,6 +144,10 @@ function removeStream(name) {
         // Remove all elements
         stream.element.remove();
         stream.menubutton.remove();
+        // Remove from focus too
+        if (document.querySelector(`#channel-focus iframe.${name.replace(/\d+/g, '')}`)) {
+            removeFocused();
+        }
         // Remove from streams list
         var index = streams.findIndex((elmnt) => elmnt.name === name);
         streams.splice(index, 1)
@@ -123,6 +163,25 @@ function removeAllStreams() {
         document.querySelector(`.menu-button.channel.${name.replace(/\d+/g, '')}`).remove();
     });
     streams = [];
+}
+// Remove focused stream
+function removeFocused() {
+    let focusElmnt = document.querySelector('#channel-focus');
+    let channelContainer = document.querySelector('.channels-container');
+
+    if (channelContainer.classList.contains('focus')) {
+        if (focusElmnt.querySelector('iframe')) {
+            if (document.querySelector('.menu-button.focus')) {
+                document.querySelector('.menu-button.focus').classList.remove('focus');
+            }
+            if (focusElmnt && focusElmnt.querySelector('iframe')) {
+                focusElmnt.querySelector('iframe').remove();
+            }
+            if (channelContainer) {
+                channelContainer.classList.remove('focus');
+            }
+        }
+    }
 }
 // Add sample streams
 function sampleStreams() {
@@ -140,30 +199,61 @@ function sampleStreams() {
     addStreams(streams);
 }
 
+// Show welcome message
+var helpTxt = `
+<li>To add a stream, use the "Add a Channel" button at the bottom left.</li>
+<li>To remove a stream, click on the "X" in a box button on the right of a channel in the left menu.</li>
+<li>To focus a stream, click on the stream on the left menu.</li>
+<li>To unfocus a stream, click on the black banner on the top of the focused stream or click on the channel highlighted in red in the channel menu.</li>
+<li>You can also remove all the streams using the "Remove all Channels" button.</li>
+<li>You can mess with the [work in progress] settings using the button at the bottom left.</li>
+<br>
+Remember, if you have any issues, <a href="${config.issuesPage}" target="_blank">report them here!</a>
+`;
+function showInfoMessage() {
+    makeWindow(
+        `<i class="fa-solid fa-circle-info"></i> Info Message.`, 
+        helpTxt
+    );
+}
+
 // ON LOAD
 window.onload = () => {
     // ######### User config #########
-    // If welcome message hasn't been skipped
-    if (!userConfig || !userConfig.THISISYOURTWITCHACCOUNTINFORMATION_DONTSHARETHIS.accessToken) {
-        makeWindow(
-            `Welcome to ktg5's MultiTTV`, 
-            `Thanks for stopping by and using MultiTTV!
-            <br>
-            MultiTTV requires you to login using your Twitch account for the best experince.
-            <br>
-            Other elements on MultiTTV may ask you to login again, like chat or trying to follow a streamer, but those are not linked with MultiTTV.
-            <br>
-            We do not track you or save any bit of information about you when you visit MultiTTV.
-            <br>
-            If you have any issues, <a href="https://github.com/ktg5/multittv/issues" target="_blank">report them here!</a>`,
-            'tolink',
-            AuthLink
-        );
-    }
-
     // If config hasn't been made
     if (!userConfig) {
         window.location.reload();
+    }
+
+    // If welcome message hasn't been skipped
+    if (userConfig) {
+        if (!userConfig.THISISYOURTWITCHACCOUNTINFORMATION_DONTSHARETHIS.accessToken) {
+            makeWindow(
+                `<i class="fa-solid fa-circle-info"></i> Before using MultiTTV...`, 
+                `Thanks for stopping by and using MultiTTV!
+                <br>
+                MultiTTV requires you to login using your Twitch account for the best experince.
+                <br>
+                Other elements on MultiTTV may ask you to login again, like chat or trying to follow a streamer, but those are not linked with MultiTTV.
+                <br>
+                We do not track you or save any bit of information about you when you visit MultiTTV.
+                <br>
+                If you have any issues, <a href="${config.issuesPage}" target="_blank">report them here!</a>`,
+                'tolink',
+                AuthLink
+            );
+        } else if (!userConfig.welcomeMessage || userConfig.welcomeMessage !== true) {
+            makeWindow(
+                `<i class="fa-solid fa-circle-info"></i> Welcome to ktg5's MultiTTV!`, 
+                `
+                Here's the basics you'll need to know:
+                <br>
+                ${helpTxt}
+                `,
+                'accept',
+                ['welcomeMessage', true]
+            );
+        }
     }
 
     // Check if menu was collapsed
@@ -189,13 +279,29 @@ window.onload = () => {
 
     
     // ######### Buttons & Menus #########
+    // Close menus
+    // Left
     document.querySelector('menu.left .menu-close').addEventListener('click', async () => {
         if (document.querySelector('menu.left').getAttribute('case') == 'full') {
             document.querySelector('menu.left').setAttribute('case', 'closed');
             userConfig.collapsedMenu = true;
             applyConfig();
         } else {
+            document.querySelector('menu.right').setAttribute('case', 'closed');
             document.querySelector('menu.left').setAttribute('case', 'full');
+            userConfig.collapsedMenu = false;
+            applyConfig();
+        }
+    });
+    // Right
+    document.querySelector('menu.right .menu-close').addEventListener('click', async () => {
+        if (document.querySelector('menu.right').getAttribute('case') == 'full') {
+            document.querySelector('menu.right').setAttribute('case', 'closed');
+            userConfig.collapsedMenu = true;
+            applyConfig();
+        } else {
+            document.querySelector('menu.left').setAttribute('case', 'closed');
+            document.querySelector('menu.right').setAttribute('case', 'full');
             userConfig.collapsedMenu = false;
             applyConfig();
         }
@@ -203,7 +309,7 @@ window.onload = () => {
 
     // Add event listener for adding channels
     document.querySelector('menu.left .menu-button.add').addEventListener('click', async () => {
-        makeWindow('Add a Channel', 'Use the input box below to add a channel to watch, then click the button below that.', 'input', 'addStream($THISINPUT)');
+        makeWindow('<i class="fa-solid fa-square-plus" aria-hidden="true"></i> Add a Channel', 'Use the input box below to add a channel to watch, then click the button below that.', 'input', 'addStream($THISINPUT)');
     });
 
     // Add event listener for closing all channels
